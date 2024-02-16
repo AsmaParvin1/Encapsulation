e1741b6e8ff8b346816f15df9fa8b2ccf53c8e53
Asma <nasrinislam32965@gmail.com>
2024-02-15 21:41:27 -0600

    initial commit

---
+++ Encapsulation/src/demo_fb/FacebookInfo.java
@@ -0,0 +1,27 @@
+package demo_fb;
+
+public class FacebookInfo {
+  public String name="Nasrin ,age ";
+   private String marital_status="Married,Ph no:";
+   private int age=30;
+   private int phonenumber=4324255;
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+public String getMarital_status() {
+	return marital_status;
+}
+
+public int getPhonenumber() {
+	return phonenumber;
+}
+public void setPhone(int phone) {
+	this.phonenumber = phone;
+} 
+   		
+   	
+}
+
---
+++ Encapsulation/src/demo_fb/Facebookaccess.java
@@ -0,0 +1,13 @@
+package demo_fb;
+
+public class Facebookaccess {
+
+	public static void main(String[] args) {
+	FacebookInfo fb=new FacebookInfo();
+	System.out.print(fb.name );
+	System.out.println(fb.getAge());
+	System.out.print(fb.getMarital_status());
+	System.out.println(fb.getPhonenumber());
+	}
+
+}
